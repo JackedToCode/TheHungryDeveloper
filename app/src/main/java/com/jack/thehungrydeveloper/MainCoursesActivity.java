7ef24028b3415733e89e1d366d372c53d8b52232
JackedToCode <Jackm1986@gmail.com>
2021-07-18 16:45:38 +0200

    Test

--- app/src/main/java/com/jack/thehungrydeveloper/MainCoursesActivity.java
+++ app/src/main/java/com/jack/thehungrydeveloper/MainCoursesActivity.java
@@ -1,15 +1,24 @@
 package com.jack.thehungrydeveloper;
 
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.jack.thehungrydeveloper.starteractivity.Dish;
 
 public class MainCoursesActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_courses);
+
+        ListView mainCoursesList = findViewById(R.id.list_view_main_courses);
+
+        Dish[] mainCourses = {
+                new Dish("Onion and parmesan ciabatta", "Warm ciabatta filled with chargrilled onion and parmesan", 124),
+                new Dish("Spinach and banana korma", "Creamy korma made with fresh spinach and banana", 542),
+                new Dish("Pepper and mushroom maki", "Toasted seaweed wrapped around sushi rice, filled with green pepper and crimini mushroom", 153)
+        };
     }
 }
